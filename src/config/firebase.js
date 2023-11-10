c80980539564a1911957f327fff68b4e9e4e60a6
Jakub Mezulanik <mezuja@gmail.com>
2023-11-10 18:33:23 +0100

    inital setup+reactRouter,firebase, reactBootstrap

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChQrNz8Bw11wP7xgTkHLYdeDFF9_cbNGw",
+  authDomain: "work-q-44c08.firebaseapp.com",
+  projectId: "work-q-44c08",
+  storageBucket: "work-q-44c08.appspot.com",
+  messagingSenderId: "417937072439",
+  appId: "1:417937072439:web:13d70537e46da568d23ab2"
+};
+
+const app = initializeApp(firebaseConfig);
